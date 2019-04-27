acb5ff6add5a20b6da74655f0550d256824c3e87
Partiks <parthpatel2597@gmail.com>
2019-04-27 17:47:03 -0400

    Initial project template given by professor

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/Message.java
@@ -0,0 +1,40 @@
+package edu.buffalo.cse.cse486586.simpledynamo;
+
+public class Message {
+    String key;
+    String message;
+    String assigned_node;
+
+    Message(String k, String msg, String assigned_node){
+        this.key = k;
+        this.message = msg;
+        this.assigned_node = assigned_node;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAssignedNode(){ return assigned_node;  }
+
+    public static java.util.Comparator<Message> id = new java.util.Comparator<Message>(){
+        public int compare(Message m1, Message m2){
+            String m1_key = m1.getKey();
+            String m2_key = m2.getKey();
+
+            return m1_key.compareTo(m2_key);
+        }
+    };
+}
